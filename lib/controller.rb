86fa6915d8f4e0a8674bdd90c097e28424c7e1f1
Yacinth <fevriercjjc@gmail.com>
2019-01-28 16:53:35 +0100

    rescue William

--- lib/controller.rb
+++ lib/controller.rb
@@ -12,9 +12,17 @@ class ApplicationController < Sinatra::Base
 	end
 	
 	post '/gossips/new/' do
-		Gossip.new(params[:gossip_autor], params[:gossip_content]).save
-		
+		#initialise la classe gossip avec comme argument (les infos du formulaire) parametres 
+		#du fichier new_gossip.erb rentrés par l'utilisateur
+		#.save Il enregistre ces informations dans un fichier csv grâce à notre classe gossip
+		Gossip.new(params[:gossip_author], params[:gossip_content]).save
+		#redirige l'utilisateur vers la page d'acceuil
 		redirect '/'
+	end
+
+	get '/' do 
+		#permet d'insérer dans notre fichier ERB des variables que l'on utilisera dans la vue.
+		erb :index, locals: {gossips: Gossip.all}
 	end
 
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,5 +1,7 @@
 class Gossip
 
+  attr_accessor :author, :content
+
   def initialize(author, content)
     @content = content
     @author = author
@@ -9,7 +11,15 @@ class Gossip
     CSV.open("./db/gossip.csv", "ab") do |csv|
       csv << [@author,@content]
     end
-    return "ce que je veux"
   end
 
+  def self.all
+    #créé une array
+    all_gossips = []
+    #lit le contenu des csv et insere dans l'array chaque ligne du csv à l'interieur
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips  
+  end    
 end
